cff6bf313d7374ccc7405b1fa040ef77648ab206
unknown <shukumar-542>
2022-02-02 12:49:39 +0600

    true and false value

---
+++ true_False.js
@@ -0,0 +1,22 @@
+// Falsy
+//0
+//""
+//undifine
+//NaN
+//Null
+//false
+
+//Truthy
+//"0", [],{}
+
+
+
+// const string = "0";
+// const age = -4 ;
+let Name =  null;
+if(Name){
+      console.log("This is True");
+}
+else{
+      console.log("This is false");
+}
